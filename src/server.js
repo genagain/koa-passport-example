5449badf25cd344f4be6d026cf682eec6b721aae
Gen Ohta <genohta@Gens-MacBook-Air.local>
2020-05-25 15:34:00 -0700

    render index.js at /

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const next = require('next');
 const Koa = require('koa');
+const Router = require('@koa/router')
 const session = require('koa-session');
 const bodyParser = require('koa-bodyparser');
 const passport = require('koa-passport');
-
-const mainRoutes = require('./routes');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -13,6 +12,8 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = new Koa();
+  const router = new Router();
+
   // sessions
   server.keys = ['super-secret-key'];
   server.use(session(server));
@@ -26,7 +27,11 @@ app.prepare().then(() => {
   server.use(passport.session());
 
   // routes
-  server.use(mainRoutes.routes());
+  server.use(async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+  });
 
   // server
   server.listen(port, () => {
